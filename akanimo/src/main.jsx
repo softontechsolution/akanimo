97a02ec05d6d2b53d39ac0ec5078b5f2ae77cdaa
softontechsolution <softontechsolution@gmail.com>
2024-05-14 06:15:35 -0700

    tailwind and route created

--- akanimo/src/main.jsx
+++ akanimo/src/main.jsx
@@ -2,6 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter } from 'react-router-dom';
+import Music from './Components/Music/Music.jsx';
+import Business from './Components/Business/Business.jsx';
+
+const router = createBrowserRouter([
+  { path: "/", element: <App /> },
+  { path: "/music", element: <Music /> },
+  { path: "/business", element: <Business /> }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
