3342eaaa1f323737bd2b12687d553e029bfc351c
softontechsolution <softontechsolution@gmail.com>
2024-05-14 06:46:12 -0700

    Link navigation solved

--- akanimo/src/Components/Hero/Hero.jsx
+++ akanimo/src/Components/Hero/Hero.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 import './Hero.css'
 import profile_img from '../../assets/profile-pic-2.png'
 
@@ -9,8 +10,8 @@ const Hero = () => {
         <h1><span>I'm Akanimo,</span> Artist and Entreprenuer.</h1>
         <p>I make music for the soul and create innovative ideas, want to explore my mind and world? Choose Below 👇</p>
         <div className="hero-action">
-            <div className="hero-connect">My Music</div>
-            <div className="hero-resume">My Business</div>
+            <Link to='/music' className='link'><div className="hero-connect">My Music</div></Link> 
+            <Link to='/business' className='link'><div className="hero-resume">My Business</div></Link> 
         </div>
     </div>
   )
--- akanimo/src/main.jsx
+++ akanimo/src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { createBrowserRouter } from 'react-router-dom';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import Music from './Components/Music/Music.jsx';
 import Business from './Components/Business/Business.jsx';
 
@@ -14,6 +14,6 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
